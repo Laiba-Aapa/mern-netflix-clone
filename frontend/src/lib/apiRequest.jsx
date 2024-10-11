c8fee674ab599f5344d2ed7777e65c071aa61157
Laiba Asif <laibasif36@gmail.com>
2024-10-11 22:23:13 +0500

    setting

--- frontend/src/lib/apiRequest.jsx
+++ frontend/src/lib/apiRequest.jsx
@@ -1,8 +1,9 @@
 // import axios from "axios"
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: `${API_URL}/api/v1`,
   withCredentials: true,
 });
 
